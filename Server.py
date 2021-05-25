1022f49e9cc34d189ff380bba4b7ad37588b1010
MarkF-hanze <74858057+MarkF-hanze@users.noreply.github.com>
2021-05-25 22:21:50 +0200

    test

--- Server.py
+++ Server.py
@@ -1,17 +1,14 @@
-#!/usr/bin/env python
-from Bio import Entrez
-import xml.etree.cElementTree as ET
-import getopt
-
 import multiprocessing as mp
 from multiprocessing.managers import BaseManager, SyncManager
-import sys, time, queue
+import os, sys, time, queue
 
 POISONPILL = "MEMENTOMORI"
 ERROR = "DOH"
+IP = ''
+PORTNUM = 5381
 AUTHKEY = b'whathasitgotinitspocketsesss?'
 
-def make_server_manager(port, authkey, ip):
+def make_server_manager(port, authkey):
     """ Create a manager for the server, listening on the given port.
         Return a manager object with get_job_q and get_result_q methods.
     """
@@ -26,15 +23,16 @@ def make_server_manager(port, authkey, ip):
 
     QueueManager.register('get_job_q', callable=lambda: job_q)
     QueueManager.register('get_result_q', callable=lambda: result_q)
-    manager = QueueManager(address=(ip, port), authkey=authkey)
+
+    manager = QueueManager(address=('', port), authkey=authkey)
     manager.start()
     print('Server started at port %s' % port)
     return manager
 
 
-def runserver(fn, data, portnum, ip):
+def runserver(fn, data):
     # Start a shared manager server and access its queues
-    manager = make_server_manager(portnum, b'whathasitgotinitspocketsesss?', ip)
+    manager = make_server_manager(PORTNUM, b'whathasitgotinitspocketsesss?')
     shared_job_q = manager.get_job_q()
     shared_result_q = manager.get_result_q()
 
@@ -53,8 +51,7 @@ def runserver(fn, data, portnum, ip):
         try:
             result = shared_result_q.get_nowait()
             results.append(result)
-            result['result'].write(f'output/{result["job"]["arg"]}.xml')
-            print(f"Got result! {result['job']['arg']}")
+            print("Got result!", result)
             if len(results) == len(data):
                 print("Got all results!")
                 break
@@ -69,6 +66,7 @@ def runserver(fn, data, portnum, ip):
     time.sleep(5)
     print("Aaaaaand we're done for the server!")
     manager.shutdown()
+    print(results)
 
 
 def make_client_manager(ip, port, authkey):
@@ -90,8 +88,8 @@ def make_client_manager(ip, port, authkey):
     return manager
 
 
-def runclient(num_processes, portnum, ip):
-    manager = make_client_manager(ip, portnum, AUTHKEY)
+def runclient(num_processes):
+    manager = make_client_manager(IP, PORTNUM, AUTHKEY)
     job_q = manager.get_job_q()
     result_q = manager.get_result_q()
     run_workers(job_q, result_q, num_processes)
@@ -132,67 +130,15 @@ def peon(job_q, result_q):
 
 
 
-
-Entrez.api_key = 'b1d6e58f788c2d50eef028a909f4971d9508'
-Entrez.email = 'mark.leendert@gmail.com'
-
-
-def get_citations(original_id):
-    """
-    Returns the pmids of the papers this paper cites
-    """
-    handle = Entrez.efetch(db='pubmed', id=original_id, retmode='xml')
-    records = Entrez.read(handle)
-    reference_id = []
-    if len(records['PubmedArticle'][0]['PubmedData']['ReferenceList']) > 0:
-        for citation in records['PubmedArticle'][0]['PubmedData']['ReferenceList'][0]['Reference']:
-            if 'ArticleIdList' in citation:
-                reference_id.append(str(citation['ArticleIdList'][0]))
-    return reference_id
-
-
-def download_paper(download_id):
-    handle = Entrez.efetch(db='pubmed', id=download_id, retmode="xml")
-    tree = ET.parse(handle)
-    return tree
-
 if __name__ == '__main__':
-    argumentList = sys.argv[1:]
-    # python3 assignment2.py -m -p 5381 -h "localhost" -a 20 "30797674"
-    # python3 assignment2.py -n 1 -w -p 5381 -h "localhost"
-    # Options
-    options = "n:wmp:h:a:"
-    opts, args = getopt.getopt(argumentList, options)
-    modes = 0
-    for o, a in opts:
-        if o == "-n":
-            number_of_children = int(a)
-        elif o == "-p":
-            PORTNUM = int(a)
-        elif o == "-h":
-            hosts = str(a)
-            # if hosts == 'localhost':
-            #     hosts = ''
-        elif o == "-a":
-            number_of_articles = int(a)
-        elif o == '-m':
-            mode = 'server'
-            modes += 1
-        elif o == '-w':
-            mode = 'client'
-            modes += 1
-    if modes ==0 or modes >= 2:
-        print('Error')
-        sys.exit()
-    if mode == 'server':
-        data = get_citations(args[0])
-        data = data[:min(number_of_articles, len(data))]
-        server = mp.Process(target=runserver, args=(download_paper, data, PORTNUM, hosts))
-        server.start()
-        time.sleep(1)
-        server.join()
-    if mode == 'client':
-        client = mp.Process(target=runclient, args=(number_of_children, PORTNUM, hosts))
-        client.start()
-        time.sleep(1)
-        client.join()
+    def capitalize(word):
+        """Capitalizes the word you pass in and returns it"""
+        return word.upper()
+    data = ["Always", "look", "on", "the", "bright", "side", "of", "life!"]
+    server = mp.Process(target=runserver, args=(capitalize, data))
+    server.start()
+    time.sleep(1)
+    client = mp.Process(target=runclient, args=(4,))
+    client.start()
+    server.join()
+    client.join()
