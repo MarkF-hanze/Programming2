f48cb8e61d00ae0e4507fa0f3443f3ae2ce8ff17
Mark Frederiks <mlfrederiks@bioinf.nl>
2021-06-11 00:04:50 +0200

    working version

--- Assigment3/Assignment3.py
+++ Assigment3/Assignment3.py
@@ -8,8 +8,7 @@ import ast
 import time
 import socket
 import os
-import importlib
-from WatchDirectory import script1
+import importlib.util
 from collections import defaultdict
 
 
@@ -28,13 +27,16 @@ class MessageManager(object):
             get_job_q = queue.Queue()
             get_result_q = queue.Queue()
             get_active_task_q = queue.Queue()
+            get_heartbeat_q = queue.Queue()
             QueueManager.register('get_job_q', callable=lambda: get_job_q)
             QueueManager.register('get_result_q', callable=lambda: get_result_q)
             QueueManager.register('get_active_task_q', callable=lambda: get_active_task_q)
+            QueueManager.register('get_heartbeat_q', callable=lambda: get_heartbeat_q)
         elif mode == 'c':
             ServerQueueManager.register('get_job_q')
             ServerQueueManager.register('get_result_q')
             ServerQueueManager.register('get_active_task_q')
+            ServerQueueManager.register('get_heartbeat_q')
 
 
 class Results(object):
@@ -74,9 +76,13 @@ class Authkey(object):
 #      return word_count
 
 
+
+
 class Server(object):
-    def __init__(self, ip, port, worker_ips):
+    def __init__(self, ip, port, worker_ips, script):
+        self.script = script
         self.working_jobs = KeepWorkingJobs(worker_ips)
+        self.roster = Roster(worker_ips)          
         self.message_manager = MessageManager('s')
         key = Authkey()
         self.manager = QueueManager(address=(ip, port), authkey=key.get_key())
@@ -87,18 +93,18 @@ class Server(object):
 
     def runserver(self):
         # Start a shared manager server and access its queues
-        # TODO dit kan niet 2x
         shared_job_q = self.manager.get_job_q()
         shared_result_q = self.manager.get_result_q()
         shared_active_task_q = self.manager.get_active_task_q()
-        # TODO tijdelijk
-        # TODO task facotry maken met dit erin
+        shared_heartbeat_q = self.manager.get_heartbeat_q()
+        # Start heatbeat
+        shared_heartbeat_q.put(self.roster)
         ID_to_func = {}
         unique_num = 0
-        for d in script1.Data('32651208'):
+        for d in self.script.Data('32651208'):
             unique_num += 1
-            ID_to_func[unique_num] = (script1.mapper, d)
-            shared_job_q.put({'fn': script1.mapper, 'arg': d, 'ID': unique_num, 'type': 'mapper'})
+            ID_to_func[unique_num] = (self.script.mapper, d)
+            shared_job_q.put({'arg': d, 'ID': unique_num, 'type': 'mapper'})
         to_get_results = unique_num
         results = Results()
         time.sleep(2)
@@ -107,19 +113,22 @@ class Server(object):
         to_get_results_2 = None
         map_count = 0
         intermediate__results = defaultdict(list)
+        print(3) 
+        runs = 0
         while True:
+            runs += 1
             # A job got taken!
             while not shared_active_task_q.empty():
                 taken_job = shared_active_task_q.get_nowait()
                 self.working_jobs.add_job(taken_job['worker'], taken_job['job']['ID'])
                 #print(self.working_jobs.get_active_jobs())
-                # print(f"Gave job {taken_job['job']['ID']} to worker {taken_job['worker']}")
+                #print(f"Gave job {taken_job['job']['ID']} to worker {taken_job['worker']}")
                 # print(f'gave {gave_count}')
             # A job got returned!
             while not shared_result_q.empty():
                 curent_results = shared_result_q.get_nowait()
                 self.working_jobs.remove_job(curent_results['worker'], curent_results['job']['ID'])
-                #print(self.working_jobs.get_active_jobs())
+                print(self.working_jobs.get_active_jobs())
                 # Shuffling 
                 if curent_results['job']['type'] == 'mapper':
                     # TODO dit niet hardcoden
@@ -140,24 +149,37 @@ class Server(object):
                 print('GO!')
                 for d in intermediate__results:
                     unique_num += 1
-                    ID_to_func[unique_num] = (script1.reducer, d)
-                    shared_job_q.put({'fn': script1.reducer, 'arg': (d, intermediate__results[d]), 'ID': unique_num,
+                    ID_to_func[unique_num] = (self.script.reducer, d)
+                    shared_job_q.put({'arg': (d, intermediate__results[d]), 'ID': unique_num,
                                       'type': 'reducer'})
                     to_get_results_2 += 1
             if result_count == to_get_results_2:
-                print([x['result'] for x in results.get_results()])
+                #print([x['result'] for x in results.get_results()])
                 print("Got all results!")
                 break
             if timer.over_time_out():
+                print('To long no result exiting')
+                break
                 for line in iter(worker_factory.stdout.readline, ""):
                     print(line, end="")
                 for line in iter(worker_factory.stderr.readline, ""):
                     print(line, end="")
                 print('To long no result exiting')
                 break
+            # Check if workers are dead
+            print(self.roster.get_status())
             time.sleep(2)
+            try:
+                self.roster = shared_heartbeat_q.get_nowait()
+                print(self.roster.get_status())
+                print('Niet Rip')
+            except queue.Empty:
+                print('Rip')
+                pass
+            #print(self.roster.get_status())
         # Tell the client process no more data will be forthcoming
         shared_job_q.put(self.poison_pill.get_pill())
+        shared_heartbeat_q.put(self.poison_pill.get_pill())
         # Sleep a bit before shutting down the server - to give clients time to
         # realize the job queue is empty and exit in an orderly way.
         time.sleep(5)
@@ -171,7 +193,7 @@ class WorkerFactory(object):
         self.server_port = server_port
         self.all_worker = []
 
-    def setup_ssh(self, ips):
+    def setup_ssh(self, ips, path):
         for ip in ips:
             print(f'Setting up SSH {ip}')
             ssh = paramiko.SSHClient()
@@ -179,16 +201,17 @@ class WorkerFactory(object):
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(ip)
             print(f'setup_worker to connect to {self.server_ip} on {self.server_port}')
-            stdin, self.stdout, self.stderr = ssh.exec_command(
-                f'/homes/mlfrederiks/p1venv/bin/python /homes/mlfrederiks/PycharmProjects/Programming2/Assigment3/Assignment3.py  -w {ip} -h {[self.server_ip]} -p {self.server_port} -n {cores}')
+            stdin, self.stdout, self.stderr = ssh.exec_command(f'/homes/mlfrederiks/p1venv/bin/python /homes/mlfrederiks/PycharmProjects/Programming2/Assigment3/Assignment3.py '  
+            f'-w {ip} -h {[self.server_ip]} -p {self.server_port} -n {cores} -d {path[0]}')
             print('Done')
 
-    def setup_worker(self, num_processes, worker_ip):
-        worker = Worker(self.server_ip, self.server_port, num_processes, worker_ip)
+    def setup_worker(self, num_processes, worker_ip, watcher):
+        worker = Worker(self.server_ip, self.server_port, num_processes, worker_ip , watcher)
 
 
 class Worker(object):
-    def __init__(self, ip, port, num_processes, worker_ip):
+    def __init__(self, ip, port, num_processes, worker_ip, watcher):
+        self.script = watcher.get_import_file()
         self.worker_ip = worker_ip
         self.message_manager = MessageManager('c')
         key = Authkey()
@@ -201,20 +224,40 @@ class Worker(object):
             except ConnectionRefusedError:
                 if (time.time() - _timer) > 60:
                     break
+        self.heartbeat_q = self.manager.get_heartbeat_q()
         print('Client connected to %s:%s' % (ip, port))
         self.poison_pill = PoisonPill()
         self.run_workers(num_processes)
 
     def run_workers(self, num_processes):
         processes = []
+        firstrun = True
         for p in range(num_processes):
-            temP = mp.Process(target=self.peon)
+            if firstrun:
+                temP = mp.Process(target=self.return_heartbeat)
+                firstrun = False
+            else:
+                temP = mp.Process(target=self.peon)
             processes.append(temP)
             temP.start()
         print("Started %s workers!" % len(processes))
         for temP in processes:
             temP.join()
-
+            
+            
+    def return_heartbeat(self):
+        while True:
+            try:
+                roster = self.heartbeat_q.get_nowait()
+                if roster == self.poison_pill.get_pill():
+                    self.heartbeat_q.put(self.poison_pill.get_pill())
+                    return
+                roster.set_status(self.worker_ip)
+                self.heartbeat_q.put(roster)
+            except queue.Empty:
+                pass
+            time.sleep(np.random.randint(10,20))
+        
     def peon(self):
         job_q = self.manager.get_job_q()
         result_q = self.manager.get_result_q()
@@ -230,11 +273,15 @@ class Worker(object):
                     return
                 else:
                     try:
+                        if job['type'] == 'mapper':
+                            func_work = self.script.mapper
+                        elif job['type'] == 'reducer':
+                            func_work = self.script.reducer
                         print(f'started_working {job}')
                         active_task_q.put({'job': job, 'worker': self.worker_ip})
                         # TODO dit met meerdere arguments (dus 1 worker werkt met functies tegelijk)
                         print(f'Ok now really')
-                        result = job['fn'](job['arg'][0], job['arg'][1])
+                        result = func_work(job['arg'][0], job['arg'][1])
 
                         print(f'Ended job {job}')
                         timer.reset_time()
@@ -247,10 +294,23 @@ class Worker(object):
                 print("sleepytime for", my_name)
                 time.sleep(1)
 
-
+class Roster(object):
+    def __init__(self, workers):
+      self.status = {}
+      for worker in workers:
+            self.status[worker] = time.time()
+      
+    def get_status(self):
+        return self.status
+    
+    def set_status(self, worker):
+        self.status[worker] = time.time()
+    
 class KeepWorkingJobs(object):
     def __init__(self, workers):
         self.active_workers_jobs = {}
+        self.workers = workers
+        self.roster = Roster(workers)
         for worker in workers:
             self.active_workers_jobs[worker] = []
 
@@ -262,7 +322,9 @@ class KeepWorkingJobs(object):
 
     def get_active_jobs(self):
         return self.active_workers_jobs
+        
 
+        
 
 class TimeOutTimer(object):
     def __init__(self, max_time):
@@ -281,13 +343,13 @@ class WatchDirectoy(object):
         self.current = -1
         self.directory = directory
         self.all_files = []
-        self.add_files()
         self.new_files = self.all_files[:]
 
     def add_files(self):
         for filename in os.listdir(self.directory):
             if filename.endswith(".py"):
                 self.all_files.append(filename)
+                self.new_files.append(filename)
 
     def check_new_file(self):
         for filename in os.listdir(self.directory):
@@ -303,6 +365,17 @@ class WatchDirectoy(object):
 
     def get_new_files(self):
         return [f"{self.directory}/{x}" for x in self.new_files]
+    
+    def get_import_file(self, with_file=None):
+        if with_file is not None:
+            spec = importlib.util.spec_from_file_location("clientScript", f'{self.directory}/{with_file}')
+        else:
+            print(self.directory)
+            spec = importlib.util.spec_from_file_location("clientScript", self.directory)
+        script = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(script)
+        return script
+
 
 
 def string_to_list(a_string_list):
@@ -313,49 +386,53 @@ def string_to_list(a_string_list):
 
 
 if __name__ == '__main__':
-    path = '/homes/mlfrederiks/PycharmProjects/Programming2/Assigment3/WatchDirectory'
-    watcher = WatchDirectoy(path)
+    #path = '/homes/mlfrederiks/PycharmProjects/Programming2/Assigment3/WatchDirectory'
     # TODO dit laten checken doorlopen ofzo
-    watcher.check_new_file()
-    new_files = watcher.get_new_files()
-    for new_file in new_files:
-        options = "mw:h:p:n:"
-        argumentList = sys.argv[1:]
-        opts, args = getopt.getopt(argumentList, options)
-        mode = None
-        for o, a in opts:
-            if o == '-m':
-                mode = 'server'
-            elif o == '-w':
-                mode = 'worker'
-                ip_worker = a
-            elif o == '-h':
-                all_ips = string_to_list(a)
-                server_ip = all_ips[0]
-                worker_ips = all_ips[:]
-                worker_ips.remove(server_ip)
-                all_ips = str(all_ips).replace(' ', '')
-            elif o == '-p':
-                server_port = int(a)
-            elif o == '-n':
-                cores = int(a)
-
-        worker_factory = WorkerFactory(server_ip, server_port)
-        if mode == 'server':
-            worker_factory.setup_ssh(worker_ips)
-            server = Server(server_ip, server_port, worker_ips)
-        elif mode == 'worker':
-            worker_factory.setup_worker(cores, ip_worker)
-        else:
-            # Start SSH of server
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(server_ip)
-            stdin, stdout, stderr = ssh.exec_command(f'/homes/mlfrederiks/p1venv/bin/python '
-                                                     f'/homes/mlfrederiks/PycharmProjects/Programming2/Assigment3/Assignment3.py '
-                                                     f'-m -h {all_ips} -p {server_port} -n {cores}')
-            # Waarom werkt het alleen, met dit sluit die anders het script?
-            for line in iter(stdout.readline, ""):
-                print(line, end="")
-            for line in iter(stderr.readline, ""):
-                print(line, end="")
+    options = "mw:h:p:n:d:"
+    argumentList = sys.argv[1:]
+    opts, args = getopt.getopt(argumentList, options)
+    mode = None
+    for o, a in opts:
+        if o == '-m':
+            mode = 'server'
+        elif o == '-w':
+            mode = 'worker'
+            ip_worker = a
+        elif o == '-h':
+            all_ips = string_to_list(a)
+            server_ip = all_ips[0]
+            worker_ips = all_ips[:]
+            worker_ips.remove(server_ip)
+            all_ips = str(all_ips).replace(' ', '')
+        elif o == '-p':
+            server_port = int(a)
+        elif o == '-n':
+            cores = int(a)
+        elif o == '-d':
+            directory = a
+    worker_factory = WorkerFactory(server_ip, server_port)
+    if mode == 'server':
+        watcher = WatchDirectoy(directory)
+        watcher.add_files()
+        watcher.check_new_file()
+        new_files = watcher.get_new_files()
+        for file in new_files:
+            print(file)
+            worker_factory.setup_ssh(worker_ips, new_files)
+            server = Server(server_ip, server_port, worker_ips, watcher.get_import_file(file.split('/')[-1]))
+    elif mode == 'worker':
+        watcher = WatchDirectoy(directory)
+        worker_factory.setup_worker(cores, ip_worker, watcher)
+    else:
+        # Start SSH of server
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(server_ip)
+        stdin, stdout, stderr = ssh.exec_command(f'/homes/mlfrederiks/p1venv/bin/python '
+                                                 f'/homes/mlfrederiks/PycharmProjects/Programming2/Assigment3/Assignment3.py '
+                                                 f'-m -h {all_ips} -p {server_port} -n {cores} -d {directory}')
+        # Waarom werkt het alleen, met dit sluit die anders het script?
+        for line in iter(stdout.readline, ""):
+            print(line, end="")
+        for line in iter(stderr.readline, ""):
+            print(line, end="")
